bf3407d9a7d5f0173f3b126d10189052c7b72bcc
rishiavery <priya.dolli@averydennison.com>
2022-01-23 12:49:40 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+
 import './App.css';
+import First from './First'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+     <div id="container">
+  <div id="left">
+    <img src="" alt="" />
+  </div>
+  
+  <div id="right">
+    <h1 id="login">LogIn</h1><br/>
+    <input type="email" id="email" class="client-info"/>
+    <label for="email">Email</label>
+    <input type="password" id="password" class="client-info"/>
+    <label for="password">Password</label>
+    <input type="submit" id="submit" class="client-info" value="Submit"/>
+    <button class="social" id="facebook">connect with facebook</button>
+    <button class="social" id="google">connect with google</button>
+  </div>
+</div>
+
     </div>
   );
 }
---
+++ src/First.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import hero from './assets/hero.png'
+
+const First = () => {
+    return (
+        <div >
+           
+           <h1>priya</h1>
+         <button className="button"> <p className="text">Start Your Order</p></button>   
+        </div>
+    )
+}
+
+export default First
